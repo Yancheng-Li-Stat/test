a718eea2f5066e48f92589910209464697e69bf6
Yancheng Li <yli778@wisc.edu>
2024-01-30 18:23:32 -0600

    initial commit

---
+++ R/justatest.R
@@ -0,0 +1,17 @@
+#' Title of Your Function
+#'
+#' Description of what your function does. This part can span multiple lines
+#' and will appear in the Details section of the documentation.
+#'
+#' @param param1 Description of the first parameter.
+#' @param param2 Description of the second parameter.
+#' @return What the function returns.
+#' @examples
+#' testfunction(1,2) # An example of how to use the function.
+#' @export
+#' @import tidyr
+#' @import dplyr
+
+testfunction <- function(a,b){
+  return(a+b)
+}
